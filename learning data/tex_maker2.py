4aff0f06a6fe880e2df7d6293dc5538e2bc76893
southglory <51065570+southglory@users.noreply.github.com>
2020-08-27 15:51:07 +0900

    test for 0.5K sized grey tex

--- learning data/tex_maker2.py
+++ learning data/tex_maker2.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-height= 10240
-width = 10240
+height= 512
+width = 512
 blank_image = np.zeros((height,width,3), np.uint8)
-blank_image[:,:]=[230,230,0]#sky blue
-blue_tex=np.array(blank_image,dtype=np.uint8)
-print(blue_tex)
-cv2.imshow('blue',blue_tex)
-cv2.imwrite('data1/tex/_dif_10k_blue.jpg',blue_tex)
+blank_image[:,:]=[200,200,200]#grey
+grey_tex=np.array(blank_image,dtype=np.uint8)
+print(grey_tex)
+cv2.imshow('blue',grey_tex)
+cv2.imwrite('data1/tex/_dif_05k_grey.jpg',grey_tex)
